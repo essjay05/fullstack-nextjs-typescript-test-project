50cfe28519398b76bdb806ef6299edf83e59278b
Joy Serquiña <stepjoyous@gmail.com>
2023-02-28 21:11:21 -0800

    Add general styles to main home button.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className='py-8 px-48'>
+      <Link
+        className='bg-teal-500 text-black font-medium px-4 py-2 rounded-md'
+        href={'/dashboard'}>Go to the Dashboard</Link>
     </main>
   )
 }
