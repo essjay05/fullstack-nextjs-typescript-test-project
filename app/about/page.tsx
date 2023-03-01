de54cfc6720f5d18fbcb67dfda28c3fabb6270f7
Joy Serquiña <stepjoyous@gmail.com>
2023-02-28 20:50:02 -0800

    Created basic about page.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,11 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function About() {
   return (
-    <main className={styles.main}>
-      
+    <main>
+      <h1>About Page</h1>
     </main>
   )
 }
